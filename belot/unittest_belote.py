c61ac75929d1064d181bfd57f696bba599e5ed5b
Boyan Bonev <bonev1717@gmail.com>
2020-03-29 20:28:05 +0300

    Added custom sort in case built-in one doesn't work. Tests for Card __lt__ and __gt__. "declare_hand()" renamed to "announce()"

--- belot/unittest_belote.py
+++ belot/unittest_belote.py
@@ -23,6 +23,31 @@ class TestCard(unittest.TestCase):
 		self.assertEqual(card4._Card__rank, "S")
 		self.assertEqual(card4._Card__number, "A")
 
+	def test_lt_dunder(self):
+		card1 = Card(rank="C", number="7")
+		card2 = Card(rank="D", number="J")
+		card3 = Card(rank="H", number="K")
+		card4 = Card(rank="S", number="A")
+
+		self.assertFalse(card1 < card2)
+		self.assertFalse(card1 < card3)
+		self.assertFalse(card1 < card4)
+		self.assertTrue(card2 < card3)
+		self.assertFalse(card2 < card4)
+		self.assertFalse(card3 < card4)
+
+	def test_gt_dunder(self):
+		card1 = Card(rank="C", number="7")
+		card2 = Card(rank="D", number="J")
+		card3 = Card(rank="H", number="K")
+		card4 = Card(rank="S", number="A")
+
+		self.assertTrue(card1 > card2)
+		self.assertTrue(card1 > card3)
+		self.assertTrue(card1 > card4)
+		self.assertFalse(card2 > card3)
+		self.assertTrue(card2 > card4)
+		self.assertTrue(card3 > card4)
 
 class TestDeck(unittest.TestCase):
 	def test_init_dunder(self):
