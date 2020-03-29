01657695d276d59402f74a8e25c25284c9f1f1cc
Boyan Bonev <bonev1717@gmail.com>
2020-03-29 22:25:53 +0300

    Adding back code from broken merge
    1 merge deleted some code so now we copy/paste recovered it

---
+++ belot/belote.py
@@ -0,0 +1,313 @@
+import random
+import copy
+
+# Клас карта, съдържа номера на картата и боята й
+class Card:
+    
+    ranks = {"S" : 1, "D" : 2, "H" : 3, "C" : 4}
+    numbers = {"7" : 1, "8" : 2, "9" : 3, "10" : 4, "J" : 5, "Q" : 6, "K" : 7, "A" : 8}
+
+    #Инициализираме картата
+    def __init__(self, rank, number):
+        self.__rank = str(rank)
+        self.__number = str(number)
+    def __str__(self):
+        return f'{self.__number}{self.__rank}'
+    def __repr__(self):
+        return f'{self.__number}{self.__rank}'
+    def __eq__(self, other):
+        return str(self) == str(other)
+    def __lt__(self, other):
+        if self.__rank != other.__rank:
+            return Card.ranks[self.__rank] < Card.ranks[other.__rank]
+        elif self.__number != other.__number:
+            return Card.numbers[self.__number] < Card.numbers[other.__number]
+        else:
+            raise Exception ("Cards are equal in rank and value") 
+    def __hash__(self):
+        return hash(self.__rank+self.__number)
+
+    def get_number(self):
+        return self.__number
+
+  # @classmethod
+# def arrange_hand(self, hand):
+#     # Insertion sort
+#     shand = copy.deepcopy(hand)
+#     for i in range(len(shand)):
+#         buf = shand[i]
+
+#         j = i - 1
+
+#         while j >= 0:
+#             if shand[j+1] > buf:
+#                 shand[j+1] = shand[j]
+#                 j = j - 1
+#             else:
+#                 shand[j+1] = buf
+#                 break
+
+#     return shand
+
+
+#class Hand:
+#   pass
+
+#Клас тесте, представлява тесте от 32 елемента на класа Карта
+class Deck:
+    #Пълним тестето с карти
+    def __init__(self):
+        ranks = ['C', 'D', 'H', 'S']
+        numbers = ['7','8','9','10','A','Q','K','A']
+        self.cards = []
+        for rank in ranks:
+            for number in numbers:
+                self.cards.append(Card(number = number, rank = rank))
+
+    def __str__(self):
+        return f'{self.cards}'
+
+    #Функция, която разбърква тестето от карти
+    def shuffle_deck(self):
+        random.shuffle(self.cards)
+
+    #Функция, която раздава 8 карти, ако тестето не е празно
+    def get_hand(self):
+        if len(self.cards) > 0 and len(self.cards) % 8 == 0:
+            hand = self.cards[0:8]
+            del self.cards[0:8]
+            return hand
+        else:
+            raise Exeption('No more cards in the deck.')
+
+
+
+        
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+    def __str__(self):
+        return f'{self.name}'
+    def __repr__(self):
+        return f'{self.name}: {self.hand}'
+    def __eq__(self, other):
+        return self.name == other.name and self.hand == other.hand
+    def draw_hand(self, deck): 
+        self.hand = deck.get_hand()
+
+    def declaring_hand(self):
+        pass
+    #check if there is carre in the player hand and adds it in declare 
+    def check_for_carre(self):
+
+        #self.hand.sort()
+        #cards = []
+        #arr = []
+        #for i in range(len(self.hand)):
+        #    if i == 0:
+        #        arr.append([self.hand[i]])
+        #    else:
+        #        if self.hand[i] == self.hand[i-1]:
+        #            arr[-1] += [self.hand[i]]
+        #        else:
+        #            arr.append([self.hand[i]])
+        #for i in arr:
+        #    if len(i) == 4:
+        #        if (i[0]._Card__number == 'J'):
+        #            self.declare.update({'carre': 200})
+        #        if (i[0]._Card__number == '9'):
+        #            self.declare.update({'carre': 150})
+        #        if (i[0]._Card__number == '10' or i[0]._Card__number == 'Q' or i[0]._Card__number == 'K' or i[0]._Card__number == 'A'):
+        #            self.declare.update({'carre': 100})
+        #        else:
+        #            cards += i
+        #    else:
+        #        cards += i
+
+        cards = copy.deepcopy(self.hand)
+        arr_carre = []
+        to_del = []
+        numbers = {"7" : 5, "8" : 5, "9" : 0, "10" : 0, "J" : 0, "Q" : 0, "K" : 0, "A" : 0}
+        for card in self.hand:
+            numbers[card.get_number()] += 1
+
+        for key in numbers.keys():
+            if numbers[key]==4:
+                carre = ('carre', key)
+                arr_carre.append(carre)
+                for i in range(len(cards)):
+                    if cards[i].get_number() == key:
+                        to_del.append(i)
+        to_del = to_del[::-1]
+        for i in to_del:
+            del cards[i]
+        print(cards)
+
+        return (arr_carre, cards)
+
+
+
+
+        
+
+
+
+        return cards
+        
+
+class Team:
+    def __init__(self, name, player1, player2):
+        self.__name = name
+        self.player1 = copy.copy(player1)
+        self.player2 = copy.copy(player2)
+        self.__player_turn = 0 #it's 0 if it's player1's turn and 1 if it's player2's turn
+    def __str__(self):
+        return f'{self.__name}: {self.player1}, {self.player2}'
+    def __eq__(self, other):
+        return str(self) == str(other)
+    def __hash__(self):
+        return hash(self.__name)
+    def get_player(self):#return the player whose turn is now and change the value of player_turn
+        if self.__player_turn == 0:
+            self.__player_turn += 1
+            return self.player1
+        elif self.__player_turn == 1:
+            self.__player_turn -= 1
+            return self.player2
+        else:
+            raise Exception("Can't reach the player.")
+
+    def choose_player(self):#choosing random player who has to start the round
+        random_num = random.random()
+        if random_num <= 0.5:
+            self.__player_turn = 0
+        else:
+            self.__player_turn = 1
+
+    def swap_players(self):
+        buf = self.player1
+        self.player1 = self.player2
+        self.player2 = buf
+
+class Round:
+    def __init__(self, teams, score, pickRandomFirst = False):
+        self.deck = Deck()
+        self.rank = random.choice(['C', 'D', 'H', 'S', "NT", "AT"])
+        self.score = score
+        self.team1 = teams[0]
+        self.team2 = teams[1]
+
+        if pickRandomFirst:
+            self.team1.choose_player()
+
+    #This is where the round plays
+    def start(self):
+        self.deck.shuffle_deck()
+        team1.player1.draw_hand()
+        team1.player2.draw_hand()
+        team2.player1.draw_hand()
+        team2.player2.draw_hand()
+
+        announcements = {1 : "", 2 : "", 3 : "", 4 : ""}
+
+        for i in range(1,5):
+            if i % 2 == 1:
+                ann = team1.get_player().announce()
+            else:
+                ann = team2.get_player().announce()
+            announcements[i] = ann
+
+            self.score += score.calc_points(announcements)
+
+    # Archive round to JSON
+    def to_json(self):
+        pass
+
+class Game:
+
+    # Grab the 2 teams and init the scoreboard
+    def __init__ (self, team1, team2):
+        self.teams = [copy.deepcopy(team1), copy.deepcopy(team2)]
+        self.score = Score(team1, team2) #teams need to be hashable!
+
+    def swap_team_positions(self):
+        buf = self.teams[0]
+        self.teams[0] = self.teams[1]
+        self.teams[1] = buf
+
+    # Progresses the game
+    # If lastGameWinner is passed, a random player from that team opens the round
+    # This function takes care of team rearrangement
+    def play_round(self, lastGameWinner = None):
+        pickRandomFirst = False
+
+        if lastGameWinner is not None:
+            pickRandomFirst = True
+            if lastGameWinner == teams[1]:
+                self.swap_team_positions()
+
+
+        round = Round(self.teams, self.score, pickRandomFirst = pickRandomFirst)
+        round.start()
+        self.teams[0].swap_players()
+        self.swap_team_positions() 
+
+    # Check if someone has won
+    def winner(self):
+        return (
+            (self.score[self.teams[0]] > 150 or self.score[self.teams[1]] > 150) and
+            (self.score[self.teams[0]] != self.score[self.teams[1]])
+            )
+
+    # Return the winner, or raise Exeption if there isn't one
+    def get_winner(self):
+        if not winner():
+            raise Exception("Nobody won yet.")
+
+        elif score[teams[0]] > 150 and score[teams[0]] > score[teams[1]]:
+            return teams[0]
+
+        elif score[teams[1]] > 150 and score[teams[1]] > score[teams[0]]:
+            return teams[1]
+
+        else:
+            raise Exception("Nobody won.")
+
+class Score:
+
+    # Init scoreboard with teams as keys
+    def __init__ (self, team1, team2):
+        self.score = {team1: 0, team2 : 0}
+
+    # Direct access to the score dictionary
+    def __getitem__ (self, arg):
+        return self.score[arg]
+
+    def __iadd__ (self, other):
+        if len(self.score) != len(other.score):
+            raise Exception("Cannot sum scores with different number of teams.")
+
+        for key in self.score.keys():
+            if key not in other.score.keys():
+                raise Exception("Scoreboard teams don't match, impossible addition.")
+
+        for key in self.score.keys():
+            self.score[key] += other.score[key]
+
+    # A method that extracts and converts announcements into points
+    # and returns a Score object to be summed with the main score 
+    @classmethod
+    def calc_points(cls, announcements):
+        pass
+
+
+
+
+
+##############################
+def main():
+    print(Card(rank = 'C', number = 'J') < Card(rank = 'C', number = '8'))
+
+if __name__ == '__main__':
+    main()
---
+++ belot/unittest_belote.py
@@ -0,0 +1,255 @@
+import unittest
+from belote import Card, Deck, Player, Team, Round, Game, Score
+
+class TestCard(unittest.TestCase):
+	def test_init_dunder(self):
+		e = None
+
+		try:
+			card1 = Card(rank="C", number="7")
+			card2 = Card(rank="D", number="J")
+			card3 = Card(rank="H", number="K")
+			card4 = Card(rank="S", number="A")
+		except Exception as exc:
+			e = exc
+
+		self.assertIsNone(e)
+		self.assertEqual(card1._Card__rank, "C")
+		self.assertEqual(card1._Card__number, "7")
+		self.assertEqual(card2._Card__rank, "D")
+		self.assertEqual(card2._Card__number, "J")
+		self.assertEqual(card3._Card__rank, "H")
+		self.assertEqual(card3._Card__number, "K")
+		self.assertEqual(card4._Card__rank, "S")
+		self.assertEqual(card4._Card__number, "A")
+
+	def test_lt_dunder(self):
+		card1 = Card(rank="C", number="7")
+		card2 = Card(rank="D", number="J")
+		card3 = Card(rank="H", number="K")
+		card4 = Card(rank="S", number="A")
+
+		self.assertFalse(card1 < card2)
+		self.assertFalse(card1 < card3)
+		self.assertFalse(card1 < card4)
+		self.assertTrue(card2 < card3)
+		self.assertFalse(card2 < card4)
+		self.assertFalse(card3 < card4)
+
+	def test_gt_dunder(self):
+		card1 = Card(rank="C", number="7")
+		card2 = Card(rank="D", number="J")
+		card3 = Card(rank="H", number="K")
+		card4 = Card(rank="S", number="A")
+
+		self.assertTrue(card1 > card2)
+		self.assertTrue(card1 > card3)
+		self.assertTrue(card1 > card4)
+		self.assertFalse(card2 > card3)
+		self.assertTrue(card2 > card4)
+		self.assertTrue(card3 > card4)
+		
+class TestDeck(unittest.TestCase):
+	def test_init_dunder(self):
+		deck = Deck()
+		expected_result = '[7C, 8C, 9C, 10C, AC, QC, KC, AC, 7D, 8D, 9D, 10D, AD, QD, KD, AD, 7H, 8H, 9H, 10H, AH, QH, KH, AH, 7S, 8S, 9S, 10S, AS, QS, KS, AS]'
+		self.assertEqual(str(deck), expected_result)
+	def test_shuffle(self):
+		deck = Deck()
+		not_expected_result = '[7C, 8C, 9C, 10C, AC, QC, KC, AC, 7D, 8D, 9D, 10D, AD, QD, KD, AD, 7H, 8H, 9H, 10H, AH, QH, KH, AH, 7S, 8S, 9S, 10S, AS, QS, KS, AS]'
+		deck.shuffle_deck()
+		self.assertEqual(len(deck.cards), 32)
+		self.assertTrue(str(deck) != not_expected_result)
+	def test_get_one_hand(self):
+		deck = Deck()
+		hand = deck.get_hand()
+		self.assertEqual(len(hand), 8)
+		self.assertEqual(len(deck.cards), 24)
+	def test_get_two_hands(self):
+		deck = Deck()
+		hand = [deck.get_hand(), deck.get_hand()]
+		self.assertEqual(len(hand[0]),8)
+		self.assertEqual(len(hand[1]),8)
+		self.assertEqual(len(deck.cards), 16)
+	def test_get_three_hands(self):
+		deck = Deck()
+		hand = [deck.get_hand(), deck.get_hand(), deck.get_hand()]
+		self.assertEqual(len(hand[0]),8)
+		self.assertEqual(len(hand[1]),8)
+		self.assertEqual(len(hand[2]),8)
+		self.assertEqual(len(deck.cards), 8)
+	def test_get_four_hands(self):
+		deck = Deck()
+		hand = [deck.get_hand(), deck.get_hand(), deck.get_hand(), deck.get_hand()]
+		self.assertEqual(len(hand[0]),8)
+		self.assertEqual(len(hand[1]),8)
+		self.assertEqual(len(hand[2]),8)
+		self.assertEqual(len(hand[3]),8)
+		self.assertEqual(len(deck.cards), 0)
+	def test_get_four_hands(self):
+		deck = Deck()
+		hand = [deck.get_hand(), deck.get_hand(), deck.get_hand(), deck.get_hand()]
+		self.assertEqual(len(hand[0]),8)
+		self.assertEqual(len(hand[1]),8)
+		self.assertEqual(len(hand[2]),8)
+		self.assertEqual(len(hand[3]),8)
+		self.assertEqual(len(deck.cards), 0)
+
+class TestPlayer(unittest.TestCase):
+	def test_init_dunder(self):
+		e = None
+
+		try:
+			p1 = Player(name = 'Silvia')
+			p2 = Player(name = 'Boyan')
+			p3 = Player(name = 'Ivan')
+			p4 = Player(name = 'Gosho')
+		except Exception as exc:
+			e = exc
+
+		self.assertIsNone(e)
+		self.assertEqual(p1.name, 'Silvia')
+		self.assertEqual(p2.name, 'Boyan')
+		self.assertEqual(p3.name, 'Ivan')
+		self.assertEqual(p4.name, 'Gosho')
+	def test_eq_dunder(self):
+		e = None
+
+		try:
+			deck = Deck()
+
+			p1 = Player(name = 'Silvia')
+			p1.draw_hand(deck)
+
+			p2 = Player(name = 'Boyan')
+			p2twin = Player(name = 'Boyan')
+			p2.draw_hand(deck)
+			p2twin.__dict__["hand"] = p2.__dict__["hand"]
+
+			p3 = Player(name = 'Ivan')
+			p4 = Player(name = 'Silvia')
+
+		except Exception as exc:
+			e = exc
+
+		self.assertIsNone(e)
+		self.assertTrue(p1 == p1)
+		self.assertTrue(p2 == p2twin)
+		self.assertFalse(p3 == p4)
+		self.assertFalse(p1 == p4)
+
+	def test_carre(self):
+		p = Player(name = 'Lin')
+		p.hand = [Card(rank="H", number="K"),
+		Card(rank="D", number="K"),
+		Card(rank="S", number="K"),
+		Card(rank="C", number="K"),
+		Card(rank="H", number="7"),
+		Card(rank="H", number="Q"),
+		Card(rank="D", number="Q"),
+		Card(rank="H", number="J")]
+		carre, hand = p.check_for_carre()
+		self.assertEqual(carre, [('carre', 'K')])
+		self.assertEqual(hand, ['7H', 'QH', 'QD', 'JH'])
+
+		
+
+
+
+class TestTeam(unittest.TestCase):
+	
+	def test_init_dunder(self):
+		e = None
+
+		try:
+			deck = Deck()
+			h1 = deck.get_hand()
+			h2 = deck.get_hand()
+			h3 = deck.get_hand()
+			h4 = deck.get_hand()
+			p1 = Player(name = 'Silvia')
+			p1.hand = h1
+			p2 = Player(name = 'Boyan')
+			p2.hand = h2
+			p3 = Player(name = 'Ivan')
+			p3.hand = h3
+			p4 = Player(name = 'Gosho')
+			p4.hand = h4
+			team1 = Team(name = 'bat', player1 = p1, player2 = p2)
+			team2 = Team(name = 'snake', player1 = p3, player2 = p4)
+		except Exception as exc:
+			e = exc
+
+		self.assertIsNone(e)
+		self.assertEqual(team1.__dict__["_Team__name"], 'bat')
+		self.assertEqual(team1.player1, p1)
+		self.assertEqual(team1.player1.hand, h1)
+		self.assertEqual(team1.player2, p2)
+		self.assertEqual(team1.player2.hand, h2)
+		self.assertEqual(team2.__dict__["_Team__name"], 'snake')
+		self.assertEqual(team2.player1, p3)
+		self.assertEqual(team2.player1.hand, h3)
+		self.assertEqual(team2.player2, p4)
+		self.assertEqual(team2.player2.hand, h4)
+	def test_eq_dunder(self):
+		e = None
+
+		try:
+			p1 = Player(name = 'Silvia')
+			p2 = Player(name = 'Boyan')
+			p3 = Player(name = 'Ivan')
+			p4 = Player(name = 'Gosho')
+			team1 = Team(name = 'bat', player1 = p1, player2 = p2)
+			team2 = Team(name = 'snake', player1 = p3, player2 = p4)
+		except Exception as exc:
+			e = exc
+
+		self.assertIsNone(e)
+		self.assertEqual(team1, Team(name = 'bat', player1 = p1, player2 = p2))
+		self.assertEqual(team2, Team(name = 'snake', player1 = p3, player2 = p4))
+	def test_get_player(self):
+		e = None
+
+		p1 = Player(name = 'Silvia')
+		p2 = Player(name = 'Boyan')
+		team1 = Team(name = 'bat', player1 = p1, player2 = p2)
+
+		try:
+			res1 = team1.get_player()
+			res2 = team1.get_player()
+			res3 = team1.get_player()
+		except Exception as exc:
+			e = exc
+
+		self.assertEqual(res1, p1)
+		self.assertEqual(res2, p2)
+		self.assertEqual(res3, p1)
+	def test_get_player_with_corrupted_turn_member(self):
+		p1 = Player(name = 'Silvia')
+		p2 = Player(name = 'Boyan')
+		team1 = Team(name = 'bat', player1 = p1, player2 = p2)
+		team1.__dict__["_Team__player_turn"] = 69
+
+		try:
+			res1 = team1.get_player()
+		except Exception as exc:
+			e = exc
+
+		self.assertIsNotNone(e)
+		self.assertEqual(str(e), "Can't reach the player.")
+
+	def test_choose_player(self):
+		pass
+
+class TestRound(unittest.TestCase):
+	
+	def test_init_dunder(self):
+		pass
+
+
+class TestGAme(unittest.TestCase):
+	pass
+
+
+if __name__ == '__main__':
+	unittest.main()
