a9a2e726d654135f0e070c45b1a8e4b5e392e349
SilviyaVasileva <60818785+SilviyaVasileva@users.noreply.github.com>
2020-03-30 00:59:22 +0300

    Add files via upload
    added check_for_belote()

--- belot/unittest_belote.py
+++ belot/unittest_belote.py
@@ -49,28 +49,45 @@ class TestCard(unittest.TestCase):
 		self.assertTrue(card2 > card4)
 		self.assertTrue(card3 > card4)
 
+	def test_equal_rank(self):
+		card1 = Card(rank="D", number="7")
+		card2 = Card(rank="D", number="J")
+		card3 = Card(rank="S", number="K")
+		card4 = Card(rank="S", number="A")
+
+		self.assertTrue(Card.equal_rank(card3, card4))
+		self.assertFalse(Card.equal_rank(card2,card3))
+		self.assertTrue(Card.equal_rank(card1, card2))
+		self.assertFalse(Card.equal_rank(card1,card3))
+		self.assertFalse(Card.equal_rank(card1, card4))
+		self.assertFalse(Card.equal_rank(card2,card4))
+
 class TestDeck(unittest.TestCase):
 	def test_init_dunder(self):
 		deck = Deck()
 		expected_result = '[7C, 8C, 9C, 10C, AC, QC, KC, AC, 7D, 8D, 9D, 10D, AD, QD, KD, AD, 7H, 8H, 9H, 10H, AH, QH, KH, AH, 7S, 8S, 9S, 10S, AS, QS, KS, AS]'
 		self.assertEqual(str(deck), expected_result)
+	
 	def test_shuffle(self):
 		deck = Deck()
 		not_expected_result = '[7C, 8C, 9C, 10C, AC, QC, KC, AC, 7D, 8D, 9D, 10D, AD, QD, KD, AD, 7H, 8H, 9H, 10H, AH, QH, KH, AH, 7S, 8S, 9S, 10S, AS, QS, KS, AS]'
 		deck.shuffle_deck()
 		self.assertEqual(len(deck.cards), 32)
 		self.assertTrue(str(deck) != not_expected_result)
+	
 	def test_get_one_hand(self):
 		deck = Deck()
 		hand = deck.get_hand()
 		self.assertEqual(len(hand), 8)
 		self.assertEqual(len(deck.cards), 24)
+	
 	def test_get_two_hands(self):
 		deck = Deck()
 		hand = [deck.get_hand(), deck.get_hand()]
 		self.assertEqual(len(hand[0]),8)
 		self.assertEqual(len(hand[1]),8)
 		self.assertEqual(len(deck.cards), 16)
+	
 	def test_get_three_hands(self):
 		deck = Deck()
 		hand = [deck.get_hand(), deck.get_hand(), deck.get_hand()]
@@ -78,6 +95,7 @@ class TestDeck(unittest.TestCase):
 		self.assertEqual(len(hand[1]),8)
 		self.assertEqual(len(hand[2]),8)
 		self.assertEqual(len(deck.cards), 8)
+	
 	def test_get_four_hands(self):
 		deck = Deck()
 		hand = [deck.get_hand(), deck.get_hand(), deck.get_hand(), deck.get_hand()]
@@ -86,6 +104,7 @@ class TestDeck(unittest.TestCase):
 		self.assertEqual(len(hand[2]),8)
 		self.assertEqual(len(hand[3]),8)
 		self.assertEqual(len(deck.cards), 0)
+	
 	def test_get_four_hands(self):
 		deck = Deck()
 		hand = [deck.get_hand(), deck.get_hand(), deck.get_hand(), deck.get_hand()]
@@ -112,6 +131,7 @@ class TestPlayer(unittest.TestCase):
 		self.assertEqual(p2.name, 'Boyan')
 		self.assertEqual(p3.name, 'Ivan')
 		self.assertEqual(p4.name, 'Gosho')
+	
 	def test_eq_dunder(self):
 		e = None
 
@@ -177,8 +197,32 @@ class TestPlayer(unittest.TestCase):
 		self.assertIsNone(e)
 		self.assertEqual(result, expected)
 
+	def test_check_for_belote(self):
+		p = Player(name = 'Lin')
+		p.hand = [Card(rank="H", number="K"),
+		Card(rank="D", number="K"),
+		Card(rank="S", number="K"),
+		Card(rank="C", number="K"),
+		Card(rank="H", number="7"),
+		Card(rank="H", number="Q"),
+		Card(rank="D", number="Q"),
+		Card(rank="H", number="J")]
+		
+		t1 = p.check_for_belote("S")
+		t2 = p.check_for_belote("C")
+		t3 = p.check_for_belote("H")
+		t4 = p.check_for_belote("D")
+		t5 = p.check_for_belote("AT")
+		self.assertEqual(t1, list())
+		self.assertEqual(t2, list())
+		self.assertEqual(t3, [('belote','H')])
+		self.assertEqual(t4, [('belote','D')])
+		self.assertEqual(t5, [('belote','D'),('belote','H')])
+
+
+
+
 class TestTeam(unittest.TestCase):
-	
 	def test_init_dunder(self):
 		e = None
 
@@ -212,6 +256,7 @@ class TestTeam(unittest.TestCase):
 		self.assertEqual(team2.player1.hand, h3)
 		self.assertEqual(team2.player2, p4)
 		self.assertEqual(team2.player2.hand, h4)
+	
 	def test_eq_dunder(self):
 		e = None
 
@@ -228,6 +273,7 @@ class TestTeam(unittest.TestCase):
 		self.assertIsNone(e)
 		self.assertEqual(team1, Team(name = 'bat', player1 = p1, player2 = p2))
 		self.assertEqual(team2, Team(name = 'snake', player1 = p3, player2 = p4))
+	
 	def test_get_player(self):
 		e = None
 
@@ -245,6 +291,7 @@ class TestTeam(unittest.TestCase):
 		self.assertEqual(res1, p1)
 		self.assertEqual(res2, p2)
 		self.assertEqual(res3, p1)
+	
 	def test_get_player_with_corrupted_turn_member(self):
 		p1 = Player(name = 'Silvia')
 		p2 = Player(name = 'Boyan')
