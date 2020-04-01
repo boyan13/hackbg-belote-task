cbc67fba649c2c6e81fdcc2d4c6f56bdbcf39167
Boyan Bonev <bonev1717@gmail.com>
2020-04-01 18:32:14 +0300

    New things

--- test_Game.py
+++ test_Game.py
@@ -209,6 +209,237 @@ class test_Game(unittest.TestCase):
         self.assertIsNotNone(e)
         self.assertEqual(str(e), "Cannot create a game without players!")
 
+    def test_shift_player_order_once(self):
+        e = None
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Marto", "Team2")
+        player4 = Player("Gosho", "Team2")
+        players = [[player1, player2], [player3, player4]]
+
+        try:
+            g = Belote(players)
+            g.shift_player_order()
+        except Exception as exc:
+            e = exc 
+
+        self.assertIsNone(e)
+        self.assertEqual(g.__dict__["order"], [1, 2, 3, 0])
+
+    def test_shift_player_order_three_times(self):
+        e = None
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Marto", "Team2")
+        player4 = Player("Gosho", "Team2")
+        players = [[player1, player2], [player3, player4]]
+
+        try:
+            g = Belote(players)
+            g.shift_player_order()
+            g.shift_player_order()
+            g.shift_player_order()
+        except Exception as exc:
+            e = exc 
+
+        self.assertIsNone(e)
+        self.assertEqual(g.__dict__["order"], [3, 0, 1, 2])
+
+    def test_shift_player_order_four_times(self):
+        e = None
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Marto", "Team2")
+        player4 = Player("Gosho", "Team2")
+        players = [[player1, player2], [player3, player4]]
+
+        try:
+            g = Belote(players)
+            g.shift_player_order()
+            g.shift_player_order()
+            g.shift_player_order()
+            g.shift_player_order()
+        except Exception as exc:
+            e = exc 
+
+        self.assertIsNone(e)
+        self.assertEqual(g.__dict__["order"], [0, 1, 2, 3])
+
+    def test_pick_first(self):
+        e = None
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Marto", "Team2")
+        player4 = Player("Gosho", "Team2")
+        players = [[player1, player2], [player3, player4]]
+
+        try:
+            g = Belote(players)
+            g.pick_first_from("Team2")
+            first = g.__dict__["order"][0]
+            first = g.__dict__["players"][first]
+            third = g.__dict__["order"][2]
+            third = g.__dict__["players"][third]
+        except Exception as exc:
+            e = exc
+
+        self.assertIsNone(e)
+        self.assertEqual(first.team, "Team2")
+        self.assertEqual(third.team, "Team2")
+
+    def test_pick_first_with_nonexistent_team(self):
+        e = None
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Marto", "Team2")
+        player4 = Player("Gosho", "Team2")
+        players = [[player1, player2], [player3, player4]]
+
+        try:
+            g = Belote(players)
+            g.pick_first_from("Team3")
+        except ValueError as exc:
+            e = exc
+
+        self.assertIsNotNone(e)
+        self.assertEqual(str(e), "Team not found.")
+
+    def test_assign_hands(self):
+        e = None
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Marto", "Team2")
+        player4 = Player("Gosho", "Team2")
+        players = [[player1, player2], [player3, player4]]
+
+        try:
+            g = Belote(players)
+            g.assign_hands()
+        except Exception as exc:
+            e = exc
+
+        self.assertIsNone(e)
+        self.assertTrue(player1.__dict__["hand"] != [])
+        self.assertTrue(player2.__dict__["hand"] != [])
+        self.assertTrue(player3.__dict__["hand"] != [])
+        self.assertTrue(player4.__dict__["hand"] != [])
+        self.assertFalse(any(card in player1.hand for card in player2.hand))
+        self.assertFalse(any(card in player1.hand for card in player3.hand))
+        self.assertFalse(any(card in player1.hand for card in player4.hand))
+        self.assertFalse(any(card in player2.hand for card in player3.hand))
+        self.assertFalse(any(card in player3.hand for card in player4.hand))
+
+    def test_teams_announcements(self):
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Freeman", "Team2")
+        player4 = Player("Marto", "Team2")
+
+        players = [[player1, player2], [player3, player4]]
+        belote = Belote(players)
+
+        annoucement = {player1:[('carre', 15)], player2: [('carre', 16)], player3: [('tierce', 10)], player4: []}
+        expected_result = [
+        [{0: [('carre', 15)], 3: [('tierce', 10)]}, {0: [('carre', 16)]}],
+        [{0: [('carre', 15)]}, {0: [('carre', 16)]}, {3: [('tierce', 10)]}, {}]
+        ]
+        result= belote.get_teams_announcements(annoucement)
+        self.assertEqual(result, expected_result)
+
+    def test_final_announcement(self):
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Freeman", "Team2")
+        player4 = Player("Marto", "Team2")
+
+        players = [[player1, player2], [player3, player4]]
+        belote = Belote(players)
+
+        teams = [{0: [('carre', 10), ('carre', 12)]},{3:[('tierce', 14)]}]
+        hist = [
+        {0: [('carre', 10)]},
+        {},
+        {0: [('carre', 12),('tierce', 9)]},
+        {3: [('tierce', 14)]}
+        ]
+        expected_result = [[('carre', 10)],[],[('carre', 12)],[('tierce', 14)]]
+
+        result = belote.final_announcements(teams, hist)
+        self.assertEqual(result, expected_result)
+
+
+    def test_validate_no_announcements(self):
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Freeman", "Team2")
+        player4 = Player("Marto", "Team2")
+
+        players = [[player1, player2], [player3, player4]]
+        belote = Belote(players)
+
+        annoucement = {player1:[], player2: [], player3: [], player4: []}
+        expected_result = {player1:[], player2: [], player3: [], player4: []}
+        result = belote.validate_announcements(annoucement)
+
+    def test_validate_no_announcements_in_team1(self):
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Freeman", "Team2")
+        player4 = Player("Marto", "Team2")
+
+        players = [[player1, player2], [player3, player4]]
+        belote = Belote(players)
+
+        annoucement = {player1:[], player2: [('carre', 12), ('carre',15)], player3: [], player4: [('carre', 12), ('carre',15)]}
+        expected_result = {player1:[], player2: [('carre', 12), ('carre',15)], player3: [], player4: [('carre', 12), ('carre',15)]}
+        result = belote.validate_announcements(annoucement)
+
+    def test_validate_no_announcements_in_team2(self):
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Freeman", "Team2")
+        player4 = Player("Marto", "Team2")
+
+        players = [[player1, player2], [player3, player4]]
+        belote = Belote(players)
+
+        annoucement = {player1:[('carre', 12), ('carre',15)], player2: [], player3: [('carre', 12), ('carre',15)], player4: []}
+        expected_result = {player1:[('carre', 12), ('carre',15)], player2: [], player3: [('carre', 12), ('carre',15)], player4: []}
+        result = belote.validate_announcements(annoucement)
+
+
+    def test_validate(self):
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Freeman", "Team2")
+        player4 = Player("Marto", "Team2")
+
+        players = [[player1, player2], [player3, player4]]
+        belote = Belote(players)
+
+        annoucement = {player1:[('carre', 12), ('carre',15)], player2: [('carre', 16)], player3: [('tierce', 10)], player4: []}
+        expected_result = {player1:[], player2: [('carre', 11)], player3: [('carre', 12), ('carre',15)], player4: []}
+        result = belote.validate_announcements(annoucement)
+
+    def test__validate(self):
+        player1 = Player("Boyan", "Team1")
+        player2 = Player("Silvia", "Team1")
+        player3 = Player("Freeman", "Team2")
+        player4 = Player("Marto", "Team2")
+
+        players = [[player1, player2], [player3, player4]]
+        belote = Belote(players)
+
+        annoucement = {player1:[('carre', 12), ('carre',15)], player2: [('carre', 16)], player3: [('tierce', 10)], player4: [('tierce', 11), ('belote', 2)]}
+        expected_result = {player1:[], player2: [('carre', 16)], player3: [], player4: [('tierce', 11), ('belote', 2)]}
+        belote.get_teams_announcements(annoucement)
+        result = belote.validate_announcements(annoucement)
+        self.assertEqual(result,expected_result)
+
+
+    def test_to_txt(self):
+        pass 
+
     def test_to_json(self):
         pass
 
